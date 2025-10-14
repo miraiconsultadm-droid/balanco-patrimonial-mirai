2aba9a7aa12987e125b96865582058274d6792e0
miraiconsultadm-droid <236455985+miraiconsultadm-droid@users.noreply.github.com>
2025-10-14 19:28:52 -0400

    Fix: Atualizar Supabase anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://vwkzrcfewxekcowbhvzf.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3a3pyY2Zld3hla2Nvd2JodnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg2NzQ4MzIsImV4cCI6MjA0NDI1MDgzMn0.xLTqYhJPQGWcJ0c5N3eDGJqQGxJDZN-cZXKkQdVZzYs'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3a3pyY2Zld3hla2Nvd2JodnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyMTIxNjMsImV4cCI6MjA3NTc4ODE2M30.gnAGQo2oLjLY2kOiqA16vOPUK-3SjgAmzqlT5wDrnaw'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
